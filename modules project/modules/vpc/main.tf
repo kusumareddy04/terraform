c791d8c65860fd0ff1ccf7ee4eecb3940267114a
kusumareddy04 <gunnamareddikusuma@gmail.com>
2025-09-10 22:42:38 +0530

    Update main.tf

--- modules project/modules/vpc/main.tf
+++ modules project/modules/vpc/main.tf
@@ -1,1 +1,20 @@
+resource "aws_vpc" "myvpc" {
+
+cidr_block = var.cidr_block
+
+tags = {
+
+name = var.vpc_name
+
+}
+
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "192.168.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+}
+
 
