4c144fdbc95f317fc2aaf4548ecec816f710f621
kusumareddy04 <gunnamareddikusuma@gmail.com>
2025-09-10 22:40:53 +0530

    Update variables.tf

--- modules project/modules/ec2-instances/variables.tf
+++ modules project/modules/ec2-instances/variables.tf
@@ -1,1 +1,31 @@
 
+variable "ami" {
+
+description = "the ami id for the instance"
+type = string
+
+}
+
+variable "instance_type" {
+
+description = "ec2 instance type"
+
+type = string
+
+}
+
+variable "subnet_id" {
+
+description = "subnet id to launch the instance"
+
+type = string
+
+}
+
+variable "instance_name" {
+
+description = "name tag for the instance"
+
+type = string
+
+}
